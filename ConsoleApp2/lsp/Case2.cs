a7d2d08647c484c0da299ea1d739d9e90e0e7595
MasterChief-96 <m1kmine@ya.ru>
2026-03-27 16:44:16 +0300

    lspcase2ispravlenie ИДБ-24-17 Салгереев
    ошибка была в том, что банк аккаунт (FrozenAccount : BankAccount) ломал все: вивдрау в замороженном счёте вообще ничего не делал, депозит вместо обычного пополнения просто писал сообщение
    
    после исправления нет подкласса, который меняет смысл методов базового класса, то есть вместо public class FrozenAccount : BankAccount стало один класс банкаккаунт, а состояние заморозки хранится через изфрозен

--- ConsoleApp2/lsp/Case2.cs
+++ ConsoleApp2/lsp/Case2.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ConsoleApp2.lsp
 {
@@ -11,62 +7,69 @@ namespace ConsoleApp2.lsp
         public class BankAccount
         {
             public string AccountNumber { get; set; } = Guid.NewGuid().ToString();
-            public double Balance { get; set; }
+            public double Balance { get; private set; }
+            public bool IsFrozen { get; private set; }
 
             public virtual void Deposit(double amount)
             {
+                if (IsFrozen)
+                    throw new InvalidOperationException($"Account {AccountNumber} is frozen. Deposit is not allowed.");
+
+                if (amount <= 0)
+                    throw new ArgumentException("Deposit amount must be greater than zero.");
+
                 Balance += amount;
-                Console.WriteLine("Deposited " + amount + " into account " + AccountNumber);
+                Console.WriteLine($"Deposited {amount} into account {AccountNumber}");
             }
 
             public virtual void Withdraw(double amount)
             {
+                if (IsFrozen)
+                    throw new InvalidOperationException($"Account {AccountNumber} is frozen. Withdrawal is not allowed.");
+
+                if (amount <= 0)
+                    throw new ArgumentException("Withdrawal amount must be greater than zero.");
+
+                if (amount > Balance)
+                    throw new InvalidOperationException("Insufficient funds.");
+
                 Balance -= amount;
-                Console.WriteLine("Withdrew " + amount + " from account " + AccountNumber);
+                Console.WriteLine($"Withdrew {amount} from account {AccountNumber}");
             }
 
             public virtual void Transfer(BankAccount targetAccount, double amount)
             {
+                if (targetAccount == null)
+                    throw new ArgumentNullException(nameof(targetAccount));
+
                 Withdraw(amount);
                 targetAccount.Deposit(amount);
-                Console.WriteLine("Transferred " + amount + " from account " + AccountNumber + " to " + targetAccount.AccountNumber);
+
+                Console.WriteLine($"Transferred {amount} from account {AccountNumber} to {targetAccount.AccountNumber}");
             }
 
             public virtual string GetAccountInfo()
             {
-                return "Account: " + AccountNumber + " with balance: " + Balance;
+                string status = IsFrozen ? "Frozen" : "Active";
+                return $"Account: {AccountNumber}, balance: {Balance}, status: {status}";
             }
 
             public virtual void UpdateAccountDetails()
             {
-                Console.WriteLine("Updating account details for " + AccountNumber);
-            }
-        }
-
-        public class FrozenAccount : BankAccount
-        {
-            public bool IsFrozen { get; set; } = true;
-
-            public override void Withdraw(double amount)
-            { }
-
-            public override void Deposit(double amount)
-            {
-                Console.WriteLine("Cannot deposit to a frozen account " + AccountNumber);
-            }
-
-            public void Unfreeze()
-            {
-                IsFrozen = false;
-                Console.WriteLine("Account " + AccountNumber + " is now unfrozen");
+                Console.WriteLine($"Updating account details for {AccountNumber}");
             }
 
             public void Freeze()
             {
                 IsFrozen = true;
-                Console.WriteLine("Account " + AccountNumber + " is frozen again");
+                Console.WriteLine($"Account {AccountNumber} is frozen");
+            }
+
+            public void Unfreeze()
+            {
+                IsFrozen = false;
+                Console.WriteLine($"Account {AccountNumber} is now unfrozen");
             }
         }
-
     }
 }
